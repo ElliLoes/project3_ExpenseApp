7c791e3e8e9165a5c9a0cba64d5bdf9f26d2c103
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2020-01-12 17:32:52 +1100

    Additional components

---
+++ client/src/components/List/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "../Image";
+import { Container, Row, Col } from "../Grid";
+
+
+export function ExpenseList({ children }) {
+  return (
+    <ul className="list-group">{children}</ul>
+  );
+}
+
+export class ExpenseListItem extends React.Component {
+
+  render() {
+    console.log(this.props);
+    return (
+      <li>
+        <Container>
+          <Row>
+            <Col size="xs-4 sm-2">
+              <Image src={this.props.image} />
+            </Col>
+            <Col size="xs-8 sm-9">
+              <h3>{this.props.title}<span><h5>{this.props.amount.join(", ")}</h5></span></h3>
+              <p>
+                {this.props.description}
+                {this.props.date}
+                {this.props.category}
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </li>
+    );
+  }
+}
+
+
