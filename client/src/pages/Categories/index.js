075ef09c3b1799fa5a02d7e316688fb72cb56188
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2020-01-20 22:04:26 +1100

    Add react-mimimal-pie-chart

--- client/src/pages/Categories/index.js
+++ client/src/pages/Categories/index.js
@@ -9,11 +9,11 @@ import ModifyBtn from "../../components/ModifyBtn";
 import AddCategoryBtn from "../../components/AddCategoryBtn";
 import Nav from "../../components/Nav";
 import "./style.css";
-
+import PieChart from 'react-minimal-pie-chart';
 // import AddExpense from "../../components/AddExpense";
 // import AddExpense from "../../pages/AddExpense";
 
-
+const colors = ['#43A19E', '#7B43A1', '#F2317A', '#FF9824', '#58CF6C'];
 
 class Categories extends Component {
     constructor(props) {
@@ -21,39 +21,54 @@ class Categories extends Component {
         this.state = {
             categories: [],
             categoryTotals: {},
-            initialized: true
-        }
+            data: []
+        };
+        this.colors = ['#43A19E', '#7B43A1', '#F2317A', '#FF9824', '#58CF6C']
     }
 
     componentDidMount() {
         this.loadData();
     }
 
+
     loadData = () => {
         API.getExpenses()
-        .then(res => {
-            // this.setState(res.data);
-            const expenses = res.data;
-            const categoryTotals = {};
-            expenses.forEach (expense => {
-                const categoryId = expense.category._id;
-                if (categoryTotals[categoryId]) {
-                  categoryTotals[categoryId] = categoryTotals[categoryId] + expense.amount;
-                } else {
-                  categoryTotals[categoryId] = expense.amount;
-                }
-              });
-            console.log('expenses:', categoryTotals);
-            API.getCategories()
-            .then(res =>
-                this.setState({ 
-                    categories: res.data,
-                    categoryTotals
-                 })
-            )
-            .catch(err => console.log(err));
-        })
-        .catch(err => console.error(err));
+            .then(res => {
+                // this.setState(res.data);
+                const expenses = res.data;
+                const categoryTotals = {};
+                expenses.forEach(expense => {
+                    const categoryId = expense.category._id;
+                    if (categoryTotals[categoryId]) {
+                        categoryTotals[categoryId].total = categoryTotals[categoryId].total + expense.amount;
+                    } else {
+                        categoryTotals[categoryId] = { title: expense.category.title, total: expense.amount };
+                    }
+                });
+                const data = Object.keys(categoryTotals).map((key, i) => {
+                    const categoryTotal = categoryTotals[key];
+                    return {
+                        title: categoryTotal.title,
+                        value: categoryTotal.total,
+                        color: this.findColor(i)
+                    };
+                });
+                console.log('expenses:', categoryTotals);
+                API.getCategories()
+                    .then(res =>
+                        this.setState({
+                            categories: res.data,
+                            categoryTotals,
+                            data
+                        })
+                    )
+                    .catch(err => console.log(err));
+            })
+            .catch(err => console.error(err));
+    }
+
+    findColor = (i) => {
+        return colors[i % colors.length]
     }
 
     render() {
@@ -65,6 +80,17 @@ class Categories extends Component {
                         Add Category
                     />
                     <Col size="md-12">
+                    <PieChart 
+                        data={this.state.data}
+                        // radius={ 150 }
+                        // hole={ 50 }
+                        // colors={ colors }
+                        // label={true}
+                        // percent={ true }
+                        // labelPosition={ 100 }
+                    />
+                    </Col>
+                    <Col size="md-12">
                         <CategoryList>
                             {this.state.categories.map(category => {
                                 return (
@@ -74,11 +100,10 @@ class Categories extends Component {
                                             description={category.description}
                                             deleted={category.deleted}
                                             user={category.user}
-                                            total={this.state.categoryTotals[category._id]}
+                                            total={this.state.categoryTotals[category._id] ? this.state.categoryTotals[category._id].total : 0}
                                         />
                                     </div>
                                 )
-
                             })}
                         </CategoryList>
                     </Col>
